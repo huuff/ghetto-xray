17bda03dab3b0e7942a3519ee174d785adea8b65
Haf <haf@protonmail.ch>
2025-08-27 19:14:58 +0200

    feat: trying to add a better input method

--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,23 @@
-#[derive(Default)]
-pub struct EntryData {
-    pub morningstar_id: String,
-    pub name: Option<String>,
+use dioxus::prelude::*;
+
+nestify::nest! {
+    #[derive(Default, Clone, Copy, PartialEq)]
+    pub struct Portfolio {
+        #>[derive(Clone, PartialEq)]
+        pub entries: Signal<Vec<pub struct PortfolioEntry {
+            pub morningstar_id: String,
+            pub name: Option<String>
+        }>>
+    }
+}
+
+impl Portfolio {
+    pub fn sample() -> Self {
+        Portfolio {
+            entries: Signal::new(vec![PortfolioEntry {
+                morningstar_id: "TEST".into(),
+                name: Some("TEST".into()),
+            }]),
+        }
+    }
 }
--- src/xray.rs
+++ src/xray.rs
@@ -1,20 +1,16 @@
-use crate::model::EntryData;
+use crate::model::Portfolio;
 use dioxus::prelude::*;
 
 #[component]
-pub fn XRayButton(entries: Signal<Vec<Signal<EntryData>>>) -> Element {
-    let url = build_url(entries);
+pub fn XRayButton(portfolio: Portfolio) -> Element {
+    let url = build_url(portfolio);
 
     rsx! {
-        a { class: "button is-primary",
-            href: url,
-            target: "_blank",
-            "Generate"
-        }
+        a { class: "button is-primary", href: url, target: "_blank", "Generate" }
     }
 }
 
-fn build_url(entries: Signal<Vec<Signal<EntryData>>>) -> String {
+fn build_url(portfolio: Portfolio) -> String {
     use itertools::Itertools as _;
 
     struct MorningstarParam {
@@ -23,11 +19,12 @@ fn build_url(entries: Signal<Vec<Signal<EntryData>>>) -> String {
         type_id: String,
     }
 
-    let params = entries
+    let params = portfolio
+        .entries
         .read()
         .iter()
         .map(|entry| MorningstarParam {
-            security_id: entry.read().morningstar_id.clone(),
+            security_id: entry.morningstar_id.clone(),
             market_value: 1000,
             type_id: "FO".into(),
         })
