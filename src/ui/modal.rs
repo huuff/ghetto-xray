73eba254de9f8712b704e45b3577117bae8134a2
Haf <haf@protonmail.ch>
2025-09-16 21:22:55 +0200

    style: less annoying modal

--- src/ui/modal.rs
+++ src/ui/modal.rs
@@ -2,6 +2,8 @@ use std::rc::Rc;
 
 use dioxus::{prelude::*, web::WebEventExt};
 
+use crate::ui::Icon;
+
 #[component]
 pub fn Modal(title: String, children: Element, is_open: Signal<bool>) -> Element {
     let mut modal_element = use_signal::<Option<Rc<MountedData>>>(|| None);
@@ -28,13 +30,13 @@ pub fn Modal(title: String, children: Element, is_open: Signal<bool>) -> Element
         dialog {
             class: "modal",
             onmounted: move |elem| modal_element.set(Some(elem.data())),
-            div { class: "modal-box",
+            div { class: "modal-box max-h-[95vh] flex flex-col",
                 form { method: "dialog",
                     button { class: "btn btn-sm btn-circle btn-ghost absolute right-2 top-2",
-                        "x"
+                        Icon { class: "fa-solid fa-x" }
                     }
                 }
-                header { class: "modal-card-head",
+                header { class: "modal-card-head flex-shrink-0",
                     p { class: "modal-card-title", {title} }
                     button {
                         class: "delete",
@@ -42,7 +44,7 @@ pub fn Modal(title: String, children: Element, is_open: Signal<bool>) -> Element
                         onclick: close,
                     }
                 }
-                section { class: "modal-card-body", {children} }
+                section { class: "modal-card-body flex-1 h-full overflow-scroll", {children} }
             }
             form { class: "modal-backdrop", method: "dialog",
                 button { "close" }
