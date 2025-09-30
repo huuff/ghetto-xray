9a1817e71f10503abfecca3b3772ccc0ca205203
Haf <76525399+huuff@users.noreply.github.com>
2025-09-30 20:23:30 +0200

    38 dx fmt in pre commit (#44)
    * ci: dx fmt for treefmt
    
    * ci: dx fmt for treefmt
    
    * ci: dx fmt for treefmt
    
    * ci: dx fmt for treefmt
    
    * ci: comment explaining the hack

--- src/ui/hero.rs
+++ src/ui/hero.rs
@@ -6,7 +6,6 @@ pub fn Hero(title: String, subtitle: Option<String>) -> Element {
         div { class: "hero bg-base-200",
             div { class: "hero-content",
                 div { class: "max-w-md",
-
                     h1 { class: "text-5xl font-bold", "{title}" }
                     if let Some(subtitle) = subtitle {
                         p { "{subtitle}" }
