0c0615e9803d13a1c700903c3b38fe4717558004
Haf <76525399+huuff@users.noreply.github.com>
2025-11-25 20:32:17 +0100

    feat: changing table row names (#62)

--- src/ui/icon.rs
+++ src/ui/icon.rs
@@ -1,9 +1,14 @@
 use dioxus::prelude::*;
 
 #[component]
-pub fn Icon(class: String) -> Element {
+pub fn Icon(class: String, onclick: Option<EventHandler>) -> Element {
+    let onclick = move |_| {
+        if let Some(onclick) = onclick {
+            onclick(());
+        }
+    };
     rsx! {
-        span { class: "icon",
+        span { class: "icon", onclick,
             i { class }
         }
     }
